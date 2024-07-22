03c52c797b86a17213d919d5500928a8bca8bb3d
Bailee Timmons <lushhollis@gmail.com>
2024-07-22 13:38:05 -0400

    Created a function to search for sunroof information

--- index.js
+++ index.js
@@ -7,32 +7,33 @@ function toggleMenu() {
 document.addEventListener("DOMContentLoaded",async function(){
     const censusTract=await getCensusTract();
     console.log(censusTract);
+    const sunroofObj=await getSunroof(censusTract);
+    console.log(sunroofObj);
 })
 
 async function getCensusTract(){
-    const street=spaceToPlus("4600 Silver Hill Rd");
-    const city=spaceToPlus("Washington");
-    const state=spaceToPlus("DC");
-    const zip=20233;
+    const street=spaceToPlus("75 9th Ave");
+    const city=spaceToPlus("New York");
+    const state=spaceToPlus("NY");
+    const zip=10011;
     const url1=`http://localhost:3500/geocode?street=${street}&city=${city}&state=${state}&zip=${zip}&benchmark=Public_AR_Current&vintage=Current_Current&layers=10&format=json`;
 
     let x=await fetch(url1,{headers:{"Accept":"application/json"}});
     let y=await x.json();
     console.log("Received Census Tract");
-    return y.result.addressMatches[0].geographies["Census Block Groups"][0].GEOID;
+    let z=await y.result.addressMatches[0].geographies["Census Block Groups"][0].GEOID;
+    z=z.toString();
+    return z.substring(0,11);
 }
 
-function getSunroof(censusTract){
-    //fetch('json.json')
-    // .then(x=>x.json())
-    // .then(y=>{
-    //     console.log(y);
-    // })
+async function getSunroof(censusTract){
+    let a=await fetch('json.json');
+    let b=await a.json();
+    let c=await b.find((e)=>e.region_name==censusTract);
+    console.log("Found object in JSON file");
+    return c;
 }
 
-
-function spaceToPlus(str) {
+function spaceToPlus(str){
     return str.replace(/ /g, '+');
-  }
-//AIzaSyDwemvwLqnfbFKGQx_ru7eC7bYkbAuSj5U
-//DON'T USE THIS
+}
